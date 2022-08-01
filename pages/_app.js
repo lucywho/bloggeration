bd04d4c1e80b1bc8252970e2bc3cf381b2680b45
Lucy Toman <lucy@toman.me.uk>
2022-08-01 17:59:09 +0200

    installs Sanity Studio and sets up test post

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Header from "Components/Header"
+import Header from "components/Header"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,41 @@
-export default function Home() {
-    return <div>index holding text</div>
+import { client } from "lib/sanity"
+
+export default function Home({ posts }) {
+    return (
+        <div className="py-8">
+            <h1 className="mt-10 text-center text-3xl mb-10 font-extrabold tracking-tight">
+                Welcome to my blog
+            </h1>
+
+            <div className="mt-20 mx-auto text-center max-w-3xl px-10">
+                {posts.map((post, index) => (
+                    <div
+                        key={index}
+                        className="mb-10 p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col"
+                    >
+                        <p className="mb-6 text-gray-400 uppercase text-sm">
+                            {new Date(post.publishedAt).toDateString().slice(4)}
+                        </p>
+                        <h3 className="text-3xl font-semibold text-gray-900">
+                            {post.title}
+                        </h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const posts =
+        await client.fetch(`*[_type == "post"] | order(publishedAt desc){
+      _id,
+      title,
+      publishedAt,
+      'slug': slug.current,
+    }`)
+
+    return {
+        props: { posts },
+    }
 }
